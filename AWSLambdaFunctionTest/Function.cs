34f480953e1228eeb1c17a2e2b6a42c3b8eafeec
Mohsin Azam Afridi <mohsin.afridi91@hotmail.com>
2021-12-14 21:45:11 +0500

    Add project files.

---
+++ AWSLambdaFunctionTest/Function.cs
@@ -0,0 +1,28 @@
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambdaFunctionTest
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(MyData myData, ILambdaContext context)
+        {
+            return $"Hello { myData.Name}".ToUpper();
+        }
+    }
+
+    public class MyData
+    {
+        public string  Name { get; set; }
+    }
+}
